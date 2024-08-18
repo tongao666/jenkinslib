344cb1410739a39bfd3cba1f2e8ada8bfb3157dc
tongao666 <148739893+tongao666@users.noreply.github.com>
2024-08-18 18:00:09 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -2,7 +2,7 @@ package org.devops
 
 //构建类型
 def Build(buildType,buildShell) {
-  def buildTools = ["mvn":"M2","ant":"ANT","grade":"GRADlE","npm":"NODE"]
+  def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADlE","npm":"NODE"]
   println("当前选择的构建类型为${buildType}")
   buildHome = tool buildTools[buildType]
 
